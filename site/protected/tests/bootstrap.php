46b60077737373fd0d71546a806083bda5222f14
Nicolas Berezin <nicolas.berezin@gmail.com>
2015-03-05 13:15:42 +0300

    unit tests

---
+++ site/protected/tests/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+// change the following paths if necessary
+#$yiic   = dirname(__FILE__) . '/../../../yii/framework/yiic.php';
+#$config = dirname(__FILE__) . '/../../protected/config/console.php';
+
+#if (!file_exists($config) || !is_readable($config)) {
+#    echo "Bad config file {$config}\nTerminate application\n";
+#    exit(1);
+#}
+
+#require_once(dirname(__FILE__) . '/../../../yii/framework/yii.php');
+#Yii::createConsoleApplication($config);
+
+
+#require_once(dirname(__DIR__) . '/tests/WebClient.php');
+#require_once(dirname(__DIR__) . '/tests/WebTestCase.php');
+
+echo 'UNIT TEST RUNNER';
